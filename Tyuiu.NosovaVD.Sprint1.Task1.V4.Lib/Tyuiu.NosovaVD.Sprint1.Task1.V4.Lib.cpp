9320601ffe27dbc5e72541431dab337764214f5e
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-09-19 08:44:09 +0500

    Tasl1

---
+++ Tyuiu.NosovaVD.Sprint1.Task1.V4.Lib/Tyuiu.NosovaVD.Sprint1.Task1.V4.Lib.cpp
@@ -0,0 +1,15 @@
+﻿// Tyuiu.NosovaVD.Sprint1.Task1.V4.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: Это пример библиотечной функции.
+class Service1 : public ISprint1Task1
+{
+	virtual int Logic(int a) override
+	{
+		return a % 10;
+	}
+};
---
+++ Tyuiu.NosovaVD.Sprint1.Task1.V4.Test/Tyuiu.NosovaVD.Sprint1.Task1.V4.Test.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.NosovaVD.Sprint1.Task1.V4.Lib/Tyuiu.NosovaVD.Sprint1.Task1.V4.Lib.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint1Task1* service1 = new Service1();
+			int a = 127;
+			int b = service1->Logic(a);
+			Assert::AreEqual(7, b);
+		}
+	};
+}
