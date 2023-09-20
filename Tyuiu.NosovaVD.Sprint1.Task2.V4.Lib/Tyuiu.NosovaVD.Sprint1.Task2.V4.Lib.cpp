334f19ab3e290f026172f60c3e64831cf0cb1803
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-09-20 19:33:50 +0500

    Task2

---
+++ Tyuiu.NosovaVD.Sprint1.Task2.V4.Lib/Tyuiu.NosovaVD.Sprint1.Task2.V4.Lib.cpp
@@ -0,0 +1,25 @@
+﻿// Tyuiu.NosovaVD.Sprint1.Task2.V4.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: Это пример библиотечной функции.
+class Service3 : public ISprint1Task2 
+{
+	virtual int LogicLong(int a) override
+	{
+		int b = a % 10;
+        int c = 0;
+        if (b % 3 == 0)
+        {
+            c = 1;
+        }
+        else
+        {
+            c = 0;
+        }
+		return c;
+	}
+};
---
+++ Tyuiu.NosovaVD.Sprint1.Task2.V4.Test/Tyuiu.NosovaVD.Sprint1.Task2.V4.Test.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.NosovaVD.Sprint1.Task2.V4.Lib/Tyuiu.NosovaVD.Sprint1.Task2.V4.Lib.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest3
+{
+	TEST_CLASS(UnitTest3)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint1Task2* service = new Service3();
+			int a = 133;
+			int b = service->LogicLong(a);
+			Assert::AreEqual(1, b);
+		}
+	};
+}
