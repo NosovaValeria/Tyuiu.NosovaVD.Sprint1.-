411bf1a260609b9acd81eb0cf639cb5a5338fbd0
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-09-25 13:15:00 +0500

    Task4

---
+++ Tyuiu.NosovaVD.Sprint1.Task4.Lib/Tyuiu.NosovaVD.Sprint1.Task4.Lib.cpp
@@ -0,0 +1,28 @@
+﻿// Tyuiu.NosovaVD.Sprint1.Task4.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../Tyuiu.Cours1.cpp/Tyuiu.Cours.cpp.cpp"
+#include <math.h>
+// TODO: Это пример библиотечной функции.
+class Service4 : public ISprint1Task4
+{
+	virtual float Vibor(float x)  override
+	{
+		float y;
+		if (x > 0 && x != 8) 
+		{
+			y = 1 - 3 * x;
+		}
+		else if (x <= 1) 
+		{
+			y = pow(x, 2) - sin(x);
+		}
+		else
+		{
+			y = cos(x);
+		}
+		return y;
+	}
+};
---
+++ Tyuiu.NosovaVD.Sprint1.Task4.Test/Tyuiu.NosovaVD.Sprint1.Task4.Test.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.NosovaVD.Sprint1.Task4.Lib/Tyuiu.NosovaVD.Sprint1.Task4.Lib.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest4
+{
+	TEST_CLASS(UnitTest4)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint1Task4* service = new Service4();
+			float x = 5;
+			float y = service->Vibor(x);
+			float otvet = -14.0;
+			Assert::AreEqual(otvet, y);
+		}
+	};
+}
